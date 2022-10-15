736b807dad121491cdd3a7a11a0cb42c65a3cd45
Наташа Михалёва <natasamihaleva@Natasas-MacBook-Pro.local>
2022-10-15 15:25:20 +0700

    new vers

---
+++ Lab2/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+
+//enum Choice {
+//    C,
+//    D
+//};
+//
+//class GameStrategy {
+//public:
+//    virtual Choice vote() = 0;
+//    virtual void update(Choice playerA, Choice playerB) = 0;
+//    virtual ~GameStrategy() = 0;
+//};
+//
+//class AlwaysCGameStrategy :: public GameStrategy{
+//
+//
+//};
+//
+////class Creator {
+////    Product* create(Product id) {
+////        switch(id)
+////            case A:
+////            case B:
+////            case C:
+////    }
+////};
+//class Product {
+//public:
+//    virtual method() = 0;
+//};
+//
+//int main() {
+//
+//    Device* c = new Call();
+//    c->Print();
+//    return 0;
+//}
+int main(int argc, char** argv) {
+
+    return 0;
+}
+// class GameStrategy
+// enum {C, D};
+// 2 метода Choice vote();
+// void update(Choice playerA, Choice playerB);
+
+
