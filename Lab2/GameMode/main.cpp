b5f1d5d4ba5f1aedc7b26ed2535c1bb7ffa8e1df
Макар Михалёв <natasamihaleva@MacBook-Pro.local>
2022-10-29 21:37:02 +0700

    create main.cpp

---
+++ Lab2/GameMode/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Game.h"
+int main(int argc, char** argv) {
+    try {
+        Game game(argc, argv);
+    } catch (std::invalid_argument const& ex) {
+        std::cout << ex.what() << '\n';
+    }
+
+    return 0;
+}
