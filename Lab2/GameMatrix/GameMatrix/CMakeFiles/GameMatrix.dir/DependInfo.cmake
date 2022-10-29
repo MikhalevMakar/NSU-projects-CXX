a623176179ce38344dfb283bff4b83c327c1d53c
Макар Михалёв <natasamihaleva@MacBook-Pro.local>
2022-10-29 21:26:50 +0700

    add Matrix

---
+++ Lab2/GameMatrix/GameMatrix/CMakeFiles/GameMatrix.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/natasamihaleva/Desktop/Lab2/GameMatrix/GameMatrix.cpp" "GameMatrix/CMakeFiles/GameMatrix.dir/GameMatrix.cpp.o" "gcc" "GameMatrix/CMakeFiles/GameMatrix.dir/GameMatrix.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
