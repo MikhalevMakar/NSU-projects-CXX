3c8791795bb8a96dc003601b06122d4bf46eb3a6
Макар Михалёв <natasamihaleva@MacBook-Pro.local>
2022-10-29 21:31:35 +0700

    create Lab2.sh

--- Lab2/Lab2.sh
+++ Lab2/Lab2.sh
@@ -2,5 +2,6 @@
 rm MakeFile
 cmake CMakeLists.txt
 make
+./Lab2 --mode=tournament --nameStrategy=ALWAYS_VOICE_C  --nameStrategy=ALWAYS_VOICE_D --nameStrategy=ALWAYS_VOICE_C  --nameStrategy=ALWAYS_VOICE_C  --steps=3
 pwd
 whoami
