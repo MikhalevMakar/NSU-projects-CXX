297ed26b4ba7e6264eb5869a7e018fa1430e8abf
Макар Михалёв <natasamihaleva@MacBook-Pro.local>
2022-10-30 17:16:57 +0700

    create first cong file and chage Lab2.sh

--- Lab2/Lab2.sh
+++ Lab2/Lab2.sh
@@ -2,6 +2,6 @@
 rm MakeFile
 cmake CMakeLists.txt
 make
-./Lab2 --mode=tournament --nameStrategy=ALWAYS_VOICE_C  --nameStrategy=ALWAYS_VOICE_D --nameStrategy=ALWAYS_VOICE_C  --nameStrategy=ALWAYS_VOICE_C  --steps=3
+./Lab2 --mode=tournament --nameStrategy=ALWAYS_VOICE_C  --nameStrategy=ALWAYS_VOICE_D  --nameStrategy=PERCENTAGE_CHOICE --configs=configFile1.txt --gameMatrix=matrix.txt
 pwd
 whoami
